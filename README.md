1621fc2a38644edcb21a29c746d374ceb3506e8f
Mattadin <m4tt4d1n@gmail.com>
2022-03-31 15:53:26 +1100

    responsiveness fixes, readme added, code explained

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Weather-Forecaster
+
+## The Project
+
+This project was designed to allow users to see a quick weekly forecast of a city for the current week. By using a city name and two letter country code, users are able to have various items populate into weekly forecast fields with relevant information for each day of the week.
+
+## Usage
+
+The project will immediately ask to use your current location, upon acceptance the various fields will populate to give you a weather forecast for your current area! Declining the current location prompt still allows users to search, but fields will simply have placeholder values until the search is completed.
+
+![screenshot](./assets/readme-screenshot.PNG)
+
+## Credits
+
+Image for background taken from unsplash.com.
+
+Weather API used from [Open Weather App](https://home.openweathermap.org/)
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+## Features
+
+**Weather forecast for any city in the world using a city name and country code.**
+**Sleek and beautiful design using a background image from unsplash.**
+**Scrolling feature for use with mobile devices and laptops for bottom forecast fields.**
+**Dynamically populating fields, inputs can be altered within the code to select which fields are most important for users.**
--- assets/java/script.js
+++ assets/java/script.js
@@ -1,3 +1,4 @@
+// General elements to be abused in further functions
 const timeEl = document.getElementById('time');
 const dateEl = document.getElementById('date');
 const currentWeatherItemsEl = document.getElementById('current-weather-items');
@@ -5,6 +6,7 @@ const weatherForecastEl = document.getElementById('weather-forecast');
 const currentTempEl = document.getElementById('current-temp');
 const apiKey = "6f5debde80c0999a2e37b79047206d74";
 
+// manually handled time elements to reduce system delays using time API such as moment (formatting to be used below)
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 setInterval(()=> {
@@ -23,8 +25,10 @@ setInterval(()=> {
 
 }, 1000);
 
+// Immediately asks the user for their current location to populate fields with relevant information with their current location
 getWeatherData();
 
+// Gives user their information based on current location
 function getWeatherData () {
   navigator.geolocation.getCurrentPosition((success)=> {
     console.log(success);
@@ -41,6 +45,7 @@ function getWeatherData () {
   })
 }
 
+// Displays the information based on the data fetched from the API from getWeatherData function
 function showWeatherData (data){
   let {humidity, uvi, sunrise, sunset, wind_speed, temp} = data.current;
 
@@ -73,18 +78,19 @@ function showWeatherData (data){
 
 
   `;
+  // loops through the bottom cards and populates the information from the fetched data of the API corresponding with each day
   let otherDayForecast = '';
   data.daily.forEach((day, idx) => {
     if (idx == 0) {
 
       currentTempEl.innerHTML = `
-      <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
+      <img src="https://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
       <div class="other">
         <div class="day">${window.moment(day.dt * 1000).format('ddd')}</div>
         <div class="temp">Day - ${day.temp.day}&#176; C</div>
         <div class="temp">Night - ${day.temp.night}&#176; C</div>
-        <div> Wind Speed ${day.wind_speed}</div>
-        <div> Humidity - ${day.humidity} </div>
+        <div class="temp"> Wind Speed- ${day.wind_speed}</div>
+        <div class="temp"> Humidity - ${day.humidity} </div>
       </div>
       `
 
@@ -92,11 +98,11 @@ function showWeatherData (data){
       otherDayForecast += `
       <div class="weather-forecast-item">
         <div class="day">${window.moment(day.dt * 1000).format('ddd')}</div>
-        <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+        <img src="https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
         <div class="temp">Day - ${day.temp.day}&#176; C</div>
         <div class="temp">Night - ${day.temp.night}&#176; C</div>
-        <div> Wind Speed ${day.wind_speed}</div>
-        <div> Humidity - ${day.humidity} </div>
+        <div class="temp"> Wind Speed ${day.wind_speed}</div>
+        <div class="temp"> Humidity - ${day.humidity} </div>
       </div>
       `;
     }
@@ -105,6 +111,7 @@ function showWeatherData (data){
   weatherForecastEl.innerHTML = otherDayForecast;
 }
 
+// Searches the API for the lat and lon of the cities for future use
 function searchWeatherData(event) {
   event.preventDefault();
   let searchVal = document.querySelector("#search-input").value;
@@ -117,6 +124,7 @@ function searchWeatherData(event) {
   
 }
 
+// Captures the data from the search and populates the cards with the information from their target city and country
 function weatherFinal(data) {
       let {lat , lon} = data[0];
       let weatherLoc = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=metric&appid=${apiKey}`
